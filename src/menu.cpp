45cf67a0f6269c9f1cdfd62d24ff59944a527d8b
dsu-jdev <dsu.jdev@gmail.com>
2016-06-29 23:17:57 +0700

    update menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -6,4 +6,7 @@ void menu() {
 	cout << "2. Nhap them mat hang" << endl;
 	cout << "3. Xoa mat hang" << endl;
 	cout << "4. Xem thong tin cac mat hang" << endl;
+	cout << "5. Sua thong tin mat hang" << endl;
+	cout << "6. Cap nhat mat hang moi" << endl;
+	cout << "7. Thoat chuong trinh" << endl;
 }
